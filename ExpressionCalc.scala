d11c6170d999da255806f9ed699d203b0f054583
pbhatt31 <118238903+pbhatt31@users.noreply.github.com>
2024-11-03 20:38:50 -0600

    Add files via upload

--- ExpressionCalc.scala
+++ ExpressionCalc.scala
@@ -1,201 +1,215 @@
+import scala.collection.mutable
 import ExpressionCalc.ExpOperation.:=
 
-import scala.collection.mutable
-
 // new language
-object PBJ:
-  // types for values
-  type Value = Double
-  type Element = String
-  type Set = mutable.Map[Element, Value]
-
-  // types for scope
+object PBJ {
+  // define the types for variables and values
+  type Value = Any
   type Environment = mutable.Map[String, Value]
   type Stack = mutable.Stack[Environment]
-  type Context = Stack ?=> Value
-  given StackVal: Stack = mutable.Stack(mutable.Map[String, Value]())
 
-// define logic
-object ExpressionCalc:
-  import PBJ.*
-//  type EnvironmentTable = mutable.Map[String, Int]
-//  type EnvironmentTableContext = EnvironmentTable ?=> Int
-//  given envTable: EnvironmentTable = mutable.Map("evenNumber" -> 2)
-//  given envTable1: EnvironmentTable = mutable.Map("evenNumber" -> 2)
+  // create type for class
+  case class Class(name: String,
+                   vars: List[ClassVar] = List(),
+                   methods: List[Method] = List(),
+                   superClass: Option[Class] = None)
 
-  def myScopeBorder(sc: => Int) :Int = sc
+  // class variable with name and type
+  case class ClassVar(name: String, varType: VarType)
 
-  myScopeBorder {
-    myScopeBorder{
-      val x = 5
-      x
-    }
-    val x = 3
-    x
+  // class variable type with name
+  case class VarType(name: String)
+
+  // class for method which has parameters and the expression
+  case class Method(name: String, 
+                    parameters: List[Parameter], 
+                    body: ExpOperation)
+
+  // class for method parameter with name and type
+  case class Parameter(name: String, paramType: VarType)
+
+  given StackVal: Stack = mutable.Stack(mutable.Map[String, Value]()) // initialize env w/ empty
+
+  // instance of class
+  def CreateNew(cls: Class): Instance = {
+    // create new env for instance and populate the variables
+    val instanceEnv = mutable.Map[String, Value]()
+    populateInheritedVars(cls, instanceEnv)
+    new Instance(cls, instanceEnv) // return instance
   }
 
-  def f(v: Int) = v+2
-  def ff(v: Int) = ???//g+2
-
-  def outerScope(param: String) = {
-    var vasu = 476
-    println(s"outer = $param")
-
-    def innerScope(param:String) = {
-      def vasuScope(param:String) = {
-        val vasu = Map("c"->5)
-        println(vasu)
-      }
-      vasu = 1
-//      val vasu = "441"
-      println(s"inner = $param")
-      println(vasu)
-      vasuScope("fff")
-    }
-    innerScope(param + "6")
-
+  // populate the variables from the superclasses
+  private def populateInheritedVars(cls: Class, env: Environment): Unit = {
+    cls.superClass.foreach(sc => populateInheritedVars(sc, env))
+    cls.vars.foreach(v => env.put(v.name, defaultForType(v.varType)))
   }
 
-  // fuzzy set
+  private def defaultForType(varType: VarType): Value = varType.name match {
+    case "int" => 0
+    case "string" => ""
+    case _ => null
+  }
+}
 
-  // enumeration for the different operations
-  enum ExpOperation:
-    case Value(v: Value) // constant value
-    case Variable(name: String) // variable
+// instance of class
+class Instance(val cls: PBJ.Class, val env: PBJ.Environment) {
+  import PBJ._
 
-    // the set operations
-    case AssignVal(name: String, expr: ExpOperation)
-    case Union(p1: ExpOperation, p2: ExpOperation)
-    case Intersect(p1: ExpOperation, p2: ExpOperation)
-    case AndVal(p1: ExpOperation, p2: ExpOperation)
-    case OrVal(p1: ExpOperation, p2: ExpOperation)
-    case NotVal(p: ExpOperation)
-    case XorVal(p1: ExpOperation, p2: ExpOperation)
-    case Scope(body: ExpOperation)
+  // method example
+  def InvokeMethod(methodName: String, args: List[(String, Value)]): Value = {
 
-    // addition and multiplication operations
-    case Add(p1: ExpOperation, p2: ExpOperation)
-    case Mult(p1: ExpOperation, p2: ExpOperation)
-//    case Sub(p1: ExpOperation, p2: ExpOperation)
-    case :=(variable: Variable, v: Value)
-    
-//    case PBJSet()
-//    case Xyz()
+    // find method in class/superclass
+    val method = cls.methods.find(_.name == methodName)
+      .orElse(cls.superClass.flatMap(_.methods.find(_.name == methodName)))
+      .getOrElse(throw new Exception(s"Method $methodName not found in class ${cls.name}"))
 
-  import ExpOperation.*
+    // method call
+    val methodEnv = mutable.Map[String, Value]()
 
-  def eval(exp: ExpOperation): Context = exp match
-    case Value(v) => v // just return the original value
-    
-    // look for variable in environment
+    // method execution
+    method.parameters.zip(args).foreach { case (param, (name, value)) =>
+      methodEnv(param.name) = value
+    }
+
+    // push method env onto stack
+    summon[Stack].push(methodEnv)
+
+    // evaluate method, pop env after and then return result of execution
+    val result = ExpressionCalc.eval(method.body)
+    summon[Stack].pop()
+
+    result
+  }
+}
+
+// expression operations
+enum ExpOperation {
+  case Value(v: PBJ.Value)
+  case Variable(name: String) 
+
+  case Assign(name: String, expr: ExpOperation)
+  case Add(p1: ExpOperation, p2: ExpOperation)
+  case Mult(p1: ExpOperation, p2: ExpOperation)
+  case Union(p1: ExpOperation, p2: ExpOperation)
+  case Intersect(p1: ExpOperation, p2: ExpOperation)
+  case AndVal(p1: ExpOperation, p2: ExpOperation)
+  case OrVal(p1: ExpOperation, p2: ExpOperation)
+  case NotVal(p: ExpOperation)
+  case XorVal(p1: ExpOperation, p2: ExpOperation)
+  case Scope(body: ExpOperation)
+
+  case Scope(body: ExpOperation) // nested scope for scoping rules
+}
+
+import PBJ._
+import ExpOperation._
+
+// evaluating expressions in methods
+object ExpressionCalc {
+  def eval(exp: ExpOperation): Value = exp match {
+    case Value(v) => v
+
     case Variable(name) =>
-      summon[Stack].find(_.contains(name)) match
-        case Some(a) => a(name)
-        case None => throw new Exception(s"$name not found")
+      summon[Stack].find(_.contains(name))
+        .map(_.apply(name))
+        .getOrElse(throw new Exception(s"Variable $name not found"))
 
-      // variable assignment
-    case AssignVal(name, expr) =>
-      val newVal = eval(expr)
-      summon[Stack].top.update(name, newVal)
-      newVal
+    case Assign(name, expr) =>
+      val value = eval(expr)
+      summon[Stack].top(name) = value
+      value
 
-      // union is max of variables
+    case Add(p1, p2) =>
+      (eval(p1), eval(p2)) match {
+        case (a: Int, b: Int) => a + b
+        case (a: Double, b: Double) => a + b
+        case _ => throw new Exception("Incompatible types for addition")
+      }
+
+    case Mult(p1, p2) =>
+      (eval(p1), eval(p2)) match {
+        case (a: Int, b: Int) => a * b
+        case (a: Double, b: Double) => a * b
+        case _ => throw new Exception("Incompatible types for multiplication")
+      }
+
     case Union(p1, p2) =>
-      Math.max(eval(p1), eval(p2))
+      (eval(p1), eval(p2)) match {
+        case (a: Set[Any], b: Set[Any]) => a union b
+        case _ => throw new Exception("Union operation requires sets")
+      }
 
-      // intersection is min. of variables
     case Intersect(p1, p2) =>
-      Math.min(eval(p1), eval(p2))
-      
-      // case for and val (multiplication)
+      (eval(p1), eval(p2)) match {
+        case (a: Set[Any], b: Set[Any]) => a intersect b
+        case _ => throw new Exception("Intersection operation requires sets")
+      }
+
     case AndVal(p1, p2) =>
-      eval(p1) * eval(p2)
-      
-      
-      // case for or val
+      (eval(p1), eval(p2)) match {
+        case (a: Boolean, b: Boolean) => a && b
+        case (a: Int, b: Int) => a & b
+        case _ => throw new Exception("AndVal operation requires booleans or integers")
+      }
+
     case OrVal(p1, p2) =>
-      Math.max(eval(p1), eval(p2))
+      (eval(p1), eval(p2)) match {
+        case (a: Boolean, b: Boolean) => a || b
+        case (a: Int, b: Int) => a | b
+        case _ => throw new Exception("OrVal operation requires booleans or integers")
+      }
 
     case NotVal(p) =>
-      1.0 - eval(p)
+      eval(p) match {
+        case a: Boolean => !a
+        case a: Int => ~a
+        case _ => throw new Exception("NotVal operation requires a boolean or integer")
+      }
 
     case XorVal(p1, p2) =>
-      Math.abs(eval(p1) - eval(p2))
+      (eval(p1), eval(p2)) match {
+        case (a: Boolean, b: Boolean) => a ^ b
+        case (a: Int, b: Int) => a ^ b
+        case _ => throw new Exception("XorVal operation requires booleans or integers")
+      }
 
-      // deal with new scope
     case Scope(body) =>
       val stackVal = summon[Stack]
-      // add new environment for scope
+
+      // push new env for scope, evaluate the expression and then pop the env
       stackVal.push(mutable.Map[String, Value]())
       val result = eval(body)
       stackVal.pop()
       result
+  }
+}
 
-    case Add(p1, p2) =>
-      Math.min(1.0, eval(p1) + eval(p2))
-  
-    case Mult(p1, p2) =>
-      eval(p1) * eval(p2)
-      
-    // test cases
-    object TestCases:
-      import org.scalatest.flatspec.AnyFlatSpec
-      import org.scalatest.matchers.should.Matchers
-      
-      class Test1 extends AnyFlatSpec with matchers {
-        behavior of "PBJ language"
-        
-        it should "test union" in {
-          val result1 = eval(Union(Value(0.2), Value(0.6)))
-          result1 shouldBe 0.6
-        }
-        
-        // test case for intersection
-        it should "test intersection" in {
-          val result2 = eval(Intersect(Value(0.5), Value(0.6)))
-          result2 shouldBe 0.5
-        }
-        
-        // test case for and
-        it should "test and" in {
-          val result3 = eval(AndVal(Value(0.5), Value(0.6)))
-          result3 shouldBe 0.30
-        }
-        
-        // test case for or
-      }
+// test cases
+object TestCases {
+  import org.scalatest.flatspec.AnyFlatSpec
+  import org.scalatest.matchers.should.Matchers
 
-    
+  class OOLangTest extends AnyFlatSpec with Matchers {
+    behavior of "PBJ Language with OOP features"
 
-    
-//
-//  def eval(exp: ExpOperation): EnvironmentTableContext = exp match
-//    case ExpOperation.Value(i) => i
-//    case ExpOperation.Variable(s) => summon[EnvironmentTable].getOrElse(s, throw new Exception("not found"))
-//    case ExpOperation.Add(p1, p2) =>
-//      while(true) do {}
-//      eval(p1) + eval(p2)
-//    case ExpOperation.Mult(p1, p2) => eval(p1) * eval(p2)
-//    case ExpOperation.Sub(p1, p2) => eval(p1) - eval(p2)
-//    case :=(v, i) =>
-//      summon[EnvironmentTable] ++= mutable.Map(v.s -> i.i)
-//      i.i
+    it should "create instances and handle inheritance" in {
+      val baseClass = Class("Base", List(ClassVar("x", VarType("int"))))
+      val derivedClass = Class("Derived", List(ClassVar("y", VarType("int"))), superClass = Some(baseClass))
 
-//  def main(args: Array[String]): Unit = {
-//    import ExpOperation.*
-//    outerScope("5")
-//
-//    println(
-//      eval(Add(Value(2),Value(7)))
-//    )
-//    println {
-//      val x = Add(Value(3),Value(5))
-//      Add(Mult(Value(2),x),Add(Value(5),Value(7)))
-//    }
-//    println(
-//      eval(:=(Variable("x"), Value(5)))
-//    )
-//    println(
-//      eval(Add(Mult(Variable("x"), Add(Value(3), Value(5))), Sub(Value(5), Value(7))))
-//    )
-//  }
+      val baseInstance = CreateNew(baseClass)
+      baseInstance.env("x") shouldBe 0
+
+      val derivedInstance = CreateNew(derivedClass)
+      derivedInstance.env("x") shouldBe 0
+      derivedInstance.env("y") shouldBe 0
+    }
+
+    it should "invoke methods with dynamic dispatch" in {
+      val baseClass = Class("Base", methods = List(Method("foo", List(Parameter("a", VarType("int"))), Add(Value(1), Variable("a")))))
+      val derivedClass = Class("Derived", methods = List(Method("foo", List(Parameter("a", VarType("int"))), Add(Value(2), Variable("a")))), superClass = Some(baseClass))
+
+      val derivedInstance = CreateNew(derivedClass)
+      derivedInstance.InvokeMethod("foo", List(("a", 3))) shouldBe 5 
+    }
+  }
+}
